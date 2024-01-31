e9dbfeae051b86654d7af99454a608fbf7acd157
srihari dinesh <dineshdinny>
2024-01-31 09:45:47 +0530

    v3

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -15,7 +15,7 @@ export default function Nav() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-warning">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">Espacito</Link>
+    <Link className="navbar-brand" to="/">Esposito</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
